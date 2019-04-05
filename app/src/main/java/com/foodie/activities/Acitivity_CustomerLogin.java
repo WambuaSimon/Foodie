d09c7f201e964fa7232a9d5a0010132a13fa9bda
WambuaSimon <simonwambua433@gmail.com>
2019-04-05 10:54:19 +0300

    customer reg

--- app/src/main/java/com/foodie/activities/Acitivity_CustomerLogin.java
+++ app/src/main/java/com/foodie/activities/Acitivity_CustomerLogin.java
@@ -1,13 +1,79 @@
 package com.foodie.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.foodie.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class Acitivity_CustomerLogin extends AppCompatActivity {
-
+    TextView code;
+    EditText phone_no, name, location, location_description;
+    Button done;
+    String code_txt, phone_no_txt, name_txt, location_txt, location_description_txt;
+    String phoneNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acitivity_customer_login);
+
+        code = findViewById(R.id.code);
+        phone_no = findViewById(R.id.phone_no);
+        location = findViewById(R.id.location);
+        name = findViewById(R.id.name);
+        location_description = findViewById(R.id.location_description);
+        done = findViewById(R.id.done);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                code_txt = code.getText().toString();
+                phone_no_txt = phone_no.getText().toString();
+                name_txt = name.getText().toString();
+                location_txt = location.getText().toString();
+                location_description_txt = location_description.getText().toString();
+
+
+
+                if (phone_no_txt.isEmpty() || phone_no_txt.length() < 9) {
+                    phone_no.setError("Enter Valid Phone Number");
+                } else if (name_txt.isEmpty()) {
+                    name.setError("Enter Your Name");
+                } else if (location_txt.isEmpty()) {
+                    location.setError("Enter Your Location");
+                }
+                phoneNumber = code_txt + phone_no_txt;
+
+
+                Intent intent = new Intent(Acitivity_CustomerLogin.this, Activity_CustomerVerification.class);
+                intent.putExtra("phonenumber", phoneNumber);
+                intent.putExtra("name", name_txt);
+                intent.putExtra("location", location_txt);
+                intent.putExtra("location_description", location_description_txt);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(this, Activity_ViewFoods.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/com/foodie/activities/Activity_CustomerVerification.java
@@ -0,0 +1,145 @@
+package com.foodie.activities;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.foodie.MainActivity;
+import com.foodie.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class Activity_CustomerVerification extends AppCompatActivity {
+
+
+    private String verificationId;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+    private EditText editText;
+    FirebaseFirestore db;
+    String name, location, location_description, phonenumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_phone);
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        progressBar = findViewById(R.id.progressbar);
+        editText = findViewById(R.id.editTextCode);
+
+        phonenumber = getIntent().getStringExtra("phonenumber");
+        name = getIntent().getStringExtra("name");
+        location = getIntent().getStringExtra("location");
+        location_description = getIntent().getStringExtra("location_description");
+        sendVerificationCode(phonenumber);
+
+        findViewById(R.id.buttonSignIn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String code = editText.getText().toString().trim();
+
+                if (code.isEmpty() || code.length() < 6) {
+
+                    editText.setError("Enter code...");
+                    editText.requestFocus();
+                    return;
+                }
+                verifyCode(code);
+            }
+        });
+
+    }
+
+    private void verifyCode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            /*SAVE DETAILS OF USER TO FIRESTORE*/
+                            Map<String, String> map = new HashMap<>();
+                            map.put("phone", phonenumber);
+                            map.put("name", name);
+                            map.put("location", location);
+                            map.put("location_description", location_description);
+                            db.collection("user").document().set(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(), "User Saved Successfully", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+
+
+                            Intent intent = new Intent(Activity_CustomerVerification.this, Activity_ViewFoods.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+
+                        } else {
+                            Toast.makeText(Activity_CustomerVerification.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void sendVerificationCode(String number) {
+        progressBar.setVisibility(View.VISIBLE);
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                number,
+                60,
+                TimeUnit.SECONDS,
+                TaskExecutors.MAIN_THREAD,
+                mCallBack
+        );
+
+    }
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            super.onCodeSent(s, forceResendingToken);
+            verificationId = s;
+        }
+
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            String code = phoneAuthCredential.getSmsCode();
+            if (code != null) {
+                editText.setText(code);
+                verifyCode(code);
+            }
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(Activity_CustomerVerification.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    };
+}
